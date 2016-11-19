9f94cb87045026da4a4d01e7f191a8e09e786502
bushan nagendra <bushan1891@gmail.com>
2016-11-18 21:30:37 -0800

    learn

--- src/components/Table/table_index.js
+++ src/components/Table/table_index.js
@@ -15,12 +15,17 @@ renderInstruction(){
 		<div className={styles.instructionContainer}>
 			<h1 className={styles.title}>Instruction</h1>
 
-			<div className={styles.iframe}>
-				<iframe src="https://jcsconsultingdemo.app.box.com/embed/s/jmlb96y0euypgrzze5cxxz29b4twyjcd" width="1050" height="550" frameborder="0" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>
+			<div className={styles.iframe}> 
+					
+				<h1 className={styles.title}>How to use the JCSEstimate App ?</h1>
+				<iframe src="https://jcsconsultingdemo.app.box.com/embed/s/d23qrds410evx3n4sykcykweuy7aa1f4" width="1040" height="400" frameborder="0" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>
 				
 			</div>
-
-
+				<div className={styles.title}>
+					For Detailed steps please go to link 
+					<a href="https://jcsconsulting.jiveon.com/docs/DOC-17276"></a>
+				</div>		
+			
 				<ul className={styles.bodyContainer}>
 					<li className={styles.detail}>
 					<div className={styles.title}>Step 1 : Create Customer</div>
